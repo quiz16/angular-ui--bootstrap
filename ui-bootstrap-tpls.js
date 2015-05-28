20edfe2f9c8f3d0c1174510e733b0af2c8916ac1
Reynald Jo Armonia <reynald.armonia@globalzeal.net>
2015-05-28 14:00:10 +0800

    fix(timepicker): do not display current time when model is empty

--- ui-bootstrap-tpls.js
+++ ui-bootstrap-tpls.js
@@ -3910,11 +3910,17 @@ angular.module('ui.bootstrap.timepicker', [])
       hours = ( hours === 0 || hours === 12 ) ? 12 : hours % 12; // Convert 24 to 12 hour system
     }
 
-    $scope.hours = keyboardChange === 'h' ? hours : pad(hours);
-    if (keyboardChange !== 'm') {
-      $scope.minutes = pad(minutes);
+    if (ngModelCtrl.$modelValue) {
+      $scope.hours = keyboardChange === 'h' ? hours : pad(hours);
+      if (keyboardChange !== 'm') {
+        $scope.minutes = pad(minutes);
+      }
+      $scope.meridian = selected.getHours() < 12 ? meridians[0] : meridians[1];
+    } else {
+      $scope.hours = null;
+      $scope.minutes = null;
+      $scope.meridian = null;
     }
-    $scope.meridian = selected.getHours() < 12 ? meridians[0] : meridians[1];
   }
 
   function addMinutes( minutes ) {
